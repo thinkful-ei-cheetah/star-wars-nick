7e0490e5161dc2da8b76f9f3c7ea0fad3f549c5a
Nick Lee <leexnick@gmail.com>
2019-04-26 22:31:11 -0700

    add smoke test in prep for initial deploy

---
+++ src/ResultList/ResultList.test.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import ResultList from './ResultList';
+
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+
+  ReactDOM.render(<ResultList />, div);
+
+  ReactDOM.unmountComponentAtNode(div);
+});
